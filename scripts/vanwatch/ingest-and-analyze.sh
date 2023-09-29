baafa824e01e9a2816fa56ac9739c3d1805d2aa7
kamangir <arash@kamangir.net>
2023-09-28 18:03:06 -0700

    doc refactor - kamangir/bolt#746

--- scripts/vanwatch/ingest-and-analyze.sh
+++ scripts/vanwatch/ingest-and-analyze.sh
@@ -9,7 +9,7 @@ function vanwatch_ingest_and_analyze() {
     if [ $(abcli_option_int "$options" help 0) == 1 ]; then
         local options="$abcli_scripts_options,count=<-1>"
         abcli_show_usage "abcli scripts source $script_name$ABCUL[$options]$ABCUL[<object-name>]$ABCUL[\"$sentence\"]$ABCUL[$args]" \
-            "paint a sentence by different artists."
+            "ingest from traffic cameras and analyze."
         return
     fi
 
