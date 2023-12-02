de89edf10c157f55cb0c16d3edb5e1c5266a7389
kamangir <arash@kamangir.net>
2023-12-02 14:30:48 -0800

    mission patch - kamangir/bolt#123

--- notebooks_and_scripts/__init__.py
+++ notebooks_and_scripts/__init__.py
@@ -1,5 +1,5 @@
 NAME = "notebooks & scripts"
 
-VERSION = "4.36.1"
+VERSION = "4.37.1"
 
 DESCRIPTION = "📜 notebooks & scripts for experiments and aws batch jobs."
--- notebooks_and_scripts/url2image/__init__.py
+++
@@ -1,3 +0,0 @@
-NAME = "url2image"
-
-from notebooks_and_scripts.url2image.functions import read_url, render_url
