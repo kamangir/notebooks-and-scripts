20bc7aa991bd310d0ac8f9fb5a43b06b435bf44d
kamangir <arash@kamangir.net>
2023-12-17 15:40:51 -0800

    roofserver - kamangir/bolt#123

--- scripts/meta/roofserver/create.sh
+++ scripts/meta/roofserver/create.sh
@@ -3,23 +3,20 @@
 function runme() {
     local options=$1
 
-    local version="1.2.1"
-
     local script_full_name="${BASH_SOURCE[0]}"
-    local script_name=$(basename "${BASH_SOURCE[0]}")
-    local script_name=${script_name%.*}
 
     local default_object_name=$(abcli_cache read roofAI_semseg_model_AIRS_o2)
 
     if [ $(abcli_option_int "$options" help 0) == 1 ]; then
-        local options="suffix=<v2>"
+        local options="suffix=<suffix>"
         abcli_meta_script_show_usage $script_full_name "[$options]$ABCUL$ABCXOP[<model-object-name>*]$ABCXOPE" \
-            "create model, endpoint_config, and endpoint to serve <model-object-name>." \
+            "create model, endpoint_config, and endpoint and serve <model-object-name> under <suffix>." \
             "default: $default_object_name"
         return
     fi
 
     local suffix=$(abcli_option "$options" suffix v2)
+    abcli_log "🔗 suffix: $suffix"
 
     local object_name=$(abcli_clarify_object $2 $default_object_name)
 
@@ -34,9 +31,13 @@ function runme() {
         endpoint,config_suffix=$suffix,suffix=$suffix \
         $object_name
 
+    local endpoint_name=endpoint-$object_name-$suffix
+    abcli_cache write \
+        roofserver-endpoint-$suffix.endpoint_name \
+        $endpoint_name
+
     roofAI_inference describe \
-        endpoint \
-        endpoint-$object_name-$suffix
+        endpoint $endpoint_name
 }
 
 runme "$@"
