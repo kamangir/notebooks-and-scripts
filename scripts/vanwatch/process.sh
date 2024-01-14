35d18f243b066662cc17c23276f076c8c3c81115
kamangir <arash@kamangir.net>
2024-01-13 19:32:53 -0800

    vanwatch.process() - kamangir/bolt#123

--- scripts/vanwatch/process.sh
+++ scripts/vanwatch/process.sh
@@ -3,13 +3,13 @@
 function runme() {
     local options=$1
 
-    local version="3.2.1"
+    local version="3.4.1"
 
     local script_name=$(abcli_script_get name)
 
     if [ $(abcli_option_int "$options" help 0) == 1 ]; then
         local options="${EOP}dryrun$EOPE"
-        abcli_script_show_usage "$script_name$ABCUL[$options]$ABCUL[$EOP$vancouver_watching_process_options$EOPE]$ABCUL[.|<object-name>]$EARGS" \
+        abcli_script_show_usage "$script_name$ABCUL[$options]$ABCUL[$EOP$vancouver_watching_process_options$EOPE]$ABCUL[.|all|<object-name>]$EARGS" \
             "process <object-name>."
         return
     fi
@@ -17,6 +17,23 @@ function runme() {
     local process_options=$2
 
     local object_name=$(abcli_clarify_object $3 .)
+
+    if [[ "$object_name" == all ]]; then
+        local published_object_name
+        for published_object_name in $(abcli_tag search \
+            ingest,published,$area,vancouver_watching \
+            --log 0 \
+            --delim space); do
+            abcli_aws_batch source \
+                name=vanwatch-process-$(abcli_string_timestamp)-$published_object_name,$options \
+                vanwatch/process \
+                "$process_options" \
+                $published_object_name \
+                "${@:4}"
+        done
+
+        return
+    fi
 
     abcli_eval "$options" \
         vancouver_watching_process \
