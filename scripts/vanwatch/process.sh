6e0f346dec8e9ca0fc3a2ca507a6ac845cfaf180
kamangir <arash@kamangir.net>
2024-01-13 17:12:48 -0800

    vanwatch.process() - kamangir/bolt#123

--- scripts/vanwatch/process.sh
+++ scripts/vanwatch/process.sh
@@ -3,13 +3,13 @@
 function runme() {
     local options=$1
 
-    local version="3.1.1"
+    local version="3.2.1"
 
     local script_name=$(abcli_script_get name)
 
     if [ $(abcli_option_int "$options" help 0) == 1 ]; then
         local options="${EOP}dryrun$EOPE"
-        abcli_script_show_usage "$script_name$ABCUL[$options]$ABCUL[$vancouver_watching_process_options]$ABCUL[.|<object-name>]$EARGS" \
+        abcli_script_show_usage "$script_name$ABCUL[$options]$ABCUL[$EOP$vancouver_watching_process_options$EOPE]$ABCUL[.|<object-name>]$EARGS" \
             "process <object-name>."
         return
     fi
