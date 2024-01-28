4011bef67c74ad5c2bf6bc9b112ca00af1b1e04a
kamangir <arash@kamangir.net>
2024-01-28 15:13:11 -0800

    sagesemseg cache_dataset - kamangir/bolt#123

--- scripts/sagesemseg/cache_dataset.sh
+++ scripts/sagesemseg/cache_dataset.sh
@@ -7,34 +7,39 @@ function runme() {
     local script_name=$(abcli_script_get name)
 
     if [ $(abcli_option_int "$options" help 0) == 1 ]; then
-        local options="dataset=pascal-voc,suffix=<v1>"
+        local options="dataset=pascal-voc,rm,suffix=<v1>"
         abcli_meta_script_show_usage $script_full_name "$EOP[$options]$EOPE" \
             "cache dataset."
         return
     fi
 
     local dataset_name=$(abcli_option "$options" dataset pascal-voc)
+    local do_rm=$(abcli_option "$options" rm 0)
     local suffix=$(abcli_option "$options" suffix v1)
 
     local dataset_object_name=$dataset_name-$suffix
 
-    abcli_log "caching $dataset_object_name"
-
     local dataset_object_path=$abcli_object_root/$dataset_object_name
     mkdir -pv $dataset_object_path
+
+    if [[ -d "$dataset_object_path/$dataset_name" ]]; then
+        abcli_log "✅ $dataset_object_name/$dataset_name/"
+        return
+    fi
+
+    abcli_log "caching $dataset_object_name"
+
     pushd $dataset_object_path >/dev/null
 
     if [[ ! -f "$dataset_name.tgz" ]]; then
         curl -O https://fast-ai-imagelocal.s3.amazonaws.com/pascal-voc.tgz
     else
-        abcli_log "✅ $dataset_name.tgz"
+        abcli_log "✅ $dataset_object_name/$dataset_name.tgz"
     fi
 
-    if [[ ! -d "$dataset_object_path/$dataset_name" ]]; then
-        tar -xvf $dataset_name.tgz
-    else
-        abcli_log "✅ $dataset_name/"
-    fi
+    tar -xvf $dataset_name.tgz
+
+    [[ "$do_rm" == 1 ]] && rm -v $dataset_name.tgz
 
     popd >/dev/null
 }
