8d412595752fd71cc36b7b74e97e8340ef1152ef
kamangir <arash@kamangir.net>
2024-01-28 14:48:32 -0800

    - kamangir/bolt#123

---
+++ scripts/sagesemseg/cache_dataset.sh
@@ -0,0 +1,42 @@
+#! /usr/bin/env bash
+
+function runme() {
+    local options=$1
+
+    local script_full_name="${BASH_SOURCE[0]}"
+    local script_name=$(abcli_script_get name)
+
+    if [ $(abcli_option_int "$options" help 0) == 1 ]; then
+        local options="dataset=pascal-voc,suffix=<v1>"
+        abcli_meta_script_show_usage $script_full_name "$EOP[$options]$EOPE" \
+            "cache dataset."
+        return
+    fi
+
+    local dataset_name=$(abcli_option "$options" dataset pascal-voc)
+    local suffix=$(abcli_option "$options" suffix v1)
+
+    local dataset_object_name=$dataset_name-$suffix
+
+    abcli_log "caching $dataset_object_name"
+
+    local dataset_object_path=$abcli_object_root/$dataset_object_name
+    pushd $dataset_object_path >/dev/null
+
+    if [[ ! -f "$dataset_name.tgz" ]]; then
+        wget -P ./ \
+            https://fast-ai-imagelocal.s3.amazonaws.com/pascal-voc.tgz
+    else
+        abcli_log "✅ $dataset_name.tgz"
+    fi
+
+    if [[ ! -d "$dataset_object_path/$dataset_name" ]]; then
+        tar -xvf $dataset_name.tgz
+    else
+        abcli_log "✅ $dataset_name/"
+    fi
+
+    popd >/dev/null
+}
+
+runme "$@"
