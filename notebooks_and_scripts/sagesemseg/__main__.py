643896d8978c664a5a06433ea2852f17012b0c5c
kamangir <arash@kamangir.net>
2024-02-09 18:39:57 -0800

    sagesemseg train - kamangir/bolt#123

--- notebooks_and_scripts/sagesemseg/__main__.py
+++ notebooks_and_scripts/sagesemseg/__main__.py
@@ -1,6 +1,7 @@
 import argparse
 from notebooks_and_scripts import VERSION
 from notebooks_and_scripts.sagesemseg.dataset import upload as upload_dataset
+from notebooks_and_scripts.sagesemseg.model import SageSemSegModel
 from abcli import logging
 import logging
 
@@ -12,10 +13,15 @@ parser = argparse.ArgumentParser(NAME, description=f"{NAME}-{VERSION}")
 parser.add_argument(
     "task",
     type=str,
-    help="upload_dataset",
+    help="train_model|upload_dataset",
 )
 parser.add_argument(
     "--dataset_object_name",
+    type=str,
+    default="",
+)
+parser.add_argument(
+    "--model_object_name",
     type=str,
     default="",
 )
@@ -25,14 +31,50 @@ parser.add_argument(
     default="",
 )
 parser.add_argument(
+    "--epochs",
+    type=int,
+    default=10,
+)
+parser.add_argument(
     "--count",
     type=int,
     default=-1,
 )
+parser.add_argument(
+    "--deploy",
+    type=int,
+    default=1,
+    help="0|1",
+)
+parser.add_argument(
+    "--delete_endpoint",
+    type=int,
+    default=1,
+    help="0|1",
+)
 args = parser.parse_args()
 
 success = False
-if args.task == "upload_dataset":
+if args.task == "train_model":
+    success = True
+
+    model = SageSemSegModel()
+
+    if not model.train(
+        dataset_object_name=args.dataset_object_name,
+        model_object_name=args.model_object_name,
+        epochs=args.epochs,
+    ):
+        success = False
+    elif args.deploy:
+        model.deploy(
+            initial_instance_count=1,
+            instance_type="ml.c5.xlarge",
+        )
+
+        if args.delete_endpoint:
+            model.delete_endpoint()
+elif args.task == "upload_dataset":
     upload_dataset(
         dataset_object_name=args.dataset_object_name,
         object_name=args.object_name,
---
+++ scripts/sagesemseg/train.sh
@@ -0,0 +1,49 @@
+#! /usr/bin/env bash
+
+function runme() {
+    local options=$1
+
+    local script_full_name="${BASH_SOURCE[0]}"
+    local script_name=$(abcli_script_get name)
+
+    abcli_scripts source - sagesemseg/consts
+
+    if [ $(abcli_option_int "$options" help 0) == 1 ]; then
+        local options="dryrun,~upload"
+        local args="[--deploy 0]$ABCUL[--delete_endpoint 0]$ABCUL[--epochs 10]"
+        abcli_meta_script_show_usage $script_full_name "$EOP[$options]$ABCUL[test|<dataset-object-name>]$ABCUL[-|<model-object-name>]$ABCUL$args$EOPE" \
+            "<dataset-object-name> -train-> <model-object-name>."
+        return
+    fi
+
+    local do_dryrun=$(abcli_option_int "$options" dryrun 0)
+    local do_upload=$(abcli_not $do_dryrun)
+
+    local dataset_object_name=$(abcli_clarify_object "$2" test)
+    if [[ "$dataset_object_name" == "test" ]]; then
+        dataset_object_name="pascal-voc-v1-debug-v2"
+        do_upload=0
+    fi
+
+    do_upload=$(abcli_option_int "$options" upload $do_upload)
+
+    local model_object_name=$(abcli_clarify_object "$3" sagesemseg-model-test-$(abcli_string_timestamp))
+
+    abcli_log "$script_name: $dataset_object_name -> $model_object_name"
+
+    abcli_relation set \
+        $dataset_object_name \
+        $model_object_name \
+        trained
+
+    abcli_eval dryrun=$do_dryrun \
+        python3 -m notebooks_and_scripts.sagesemseg train_model \
+        --dataset_object_name $dataset_object_name \
+        --model_object_name $model_object_name \
+        "${@:4}"
+
+    [[ "$do_upload" == 1 ]] &&
+        abcli_upload - $model_object_name
+}
+
+runme "$@"
