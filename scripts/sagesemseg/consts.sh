057fd3c1e005bfda1b730d09e73620e14d08afee
kamangir <arash@kamangir.net>
2024-02-16 19:17:16 -0800

    ingesting AIRS for SageMaker - kamangir/bolt#123

--- scripts/sagesemseg/consts.sh
+++ scripts/sagesemseg/consts.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/env bash
 
-export sagesemseg_cache_dataset_options="dataset=pascal-voc|<object-name>,suffix=<v1>"
+export sagesemseg_cache_dataset_options="dataset=pascal-voc,suffix=<v1>"
