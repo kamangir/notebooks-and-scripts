ca40064a369221da7efe8a71c015745f60a9fd79
kamangir <arash@kamangir.net>
2024-03-02 19:30:02 -0800

    abcli_log_list refactor - kamangir/bolt#1234

--- notebooks_and_scripts/__init__.py
+++ notebooks_and_scripts/__init__.py
@@ -3,7 +3,7 @@ import logging
 
 NAME = "notebooks & scripts"
 
-VERSION = "4.186.1"
+VERSION = "4.187.1"
 
 DESCRIPTION = "📜 notebooks for experiments & [meta] scripts for aws batch jobs and sagemaker serving."
 
