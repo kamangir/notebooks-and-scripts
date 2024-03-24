75cdd81bb52e5ea413b9bbf23b881cbc7c66966f
kamangir <arash@kamangir.net>
2024-03-23 18:12:00 -0700

    pipeline fix - kamangir/bolt#746

--- setup.py
+++ setup.py
@@ -2,10 +2,12 @@ from setuptools import setup
 
 from notebooks_and_scripts import NAME, VERSION, DESCRIPTION
 
+NAME = NAME.replace(" & ", "_and_")
+
 setup(
     name=NAME,
     author="arash@kamangir.net",
     version=VERSION,
     description=DESCRIPTION,
-    packages=[NAME.replace(" & ", "_and_")],
+    packages=[NAME],
 )
