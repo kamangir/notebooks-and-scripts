1e392a3e2c1eea2c64f2ccc4614601c557b37c9f
kamangir <arash@kamangir.net>
2024-04-12 21:44:51 -0700

    localflow run refactor - kamangir/bolt#746

--- .abcli/localflow/run.sh
+++ .abcli/localflow/run.sh
@@ -4,47 +4,60 @@ function localflow_run() {
     local options=$1
 
     if [ $(abcli_option_int "$options" help 0) == 1 ]; then
-        local options="dryrun,name=<job-name>"
+        local options="dryrun,loop,name=<job-name>"
         abcli_show_usage "localflow run$ABCUL[$options]" \
             "run localflow job."
         return
     fi
 
+    local do_loop=$(abcli_option_int "$options" loop 0)
     local job_name=$(abcli_option "$options" name)
-    [[ -z "$job_name" ]] &&
-        job_name=$(localflow_list status=SUBMITTED \
-            --log 0 \
-            --count 1)
-    if [[ -z "$job_name" ]]; then
-        abcli_log_error "-localflow: job not found."
-        return 1
-    fi
-    abcli_log "⚙️  $job_name"
 
-    abcli_tag set \
-        $job_name \
-        ~SUBMITTED,RUNNING \
-        validate
+    while true; do
+        [[ -z "$job_name" ]] &&
+            job_name=$(localflow_list status=SUBMITTED \
+                --log 0 \
+                --count 1)
 
-    abcli_download - $job_name
+        if [[ -z "$job_name" ]]; then
+            if [[ "$do_loop" == 0 ]]; then
+                abcli_log_error "-localflow: job not found."
+                return 1
+            fi
 
-    local command_line=$(abcli_metadata get \
-        key=command_line,object \
-        $job_name)
-    if [[ -z "$command_line" ]]; then
-        abcli_log_error "-localflow: $job_name: command not found."
+            abcli_sleep 10
+            continue
+        fi
+        abcli_log "⚙️  $job_name"
+
         abcli_tag set \
             $job_name \
-            ~RUNNING,FAILED \
+            ~SUBMITTED,RUNNING \
             validate
-        return 1
-    fi
 
-    abcli_eval $options, \
-        "$command_line"
+        abcli_download - $job_name
 
-    abcli_tag set \
-        $job_name \
-        ~RUNNING,SUCCEEDED \
-        validate
+        local command_line=$(abcli_metadata get \
+            key=command_line,object \
+            $job_name)
+        if [[ -z "$command_line" ]]; then
+            abcli_log_error "-localflow: $job_name: command not found."
+            abcli_tag set \
+                $job_name \
+                ~RUNNING,FAILED \
+                validate
+            return 1
+        fi
+
+        abcli_eval $options, \
+            "$command_line"
+
+        abcli_tag set \
+            $job_name \
+            ~RUNNING,SUCCEEDED \
+            validate
+
+        job_name=
+        [[ "$do_loop" == 0 ]] && break
+    done
 }
