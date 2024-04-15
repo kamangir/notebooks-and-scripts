8b81cc2c2e5fb06d72c78afa99c1de60822f737a
kamangir <arash@kamangir.net>
2024-04-14 21:47:07 -0700

    env refactor - kamangir/bolt#746

--- notebooks_and_scripts/tests/test_env.py
+++ notebooks_and_scripts/tests/test_env.py
@@ -1,5 +1,6 @@
 from abcli.tests import test_env
 from notebooks_and_scripts import env
+import pkg_resources
 
 
 def test_abcli_env():
@@ -7,4 +8,6 @@ def test_abcli_env():
 
 
 def test_notebooks_and_scripts_env():
-    assert env.LOCALFLOW_STATUS_LIST
+    assert env.LOCALFLOW_STATUS_LIST, pkg_resources.resource_listdir(
+        "notebooks_and_scripts", ""
+    )
