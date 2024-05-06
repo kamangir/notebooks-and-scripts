787b95a4fcc713c14bd069123ac6152f94e324c8
kamangir <arash@kamangir.net>
2024-05-05 19:05:32 -0700

    += workflow pattern tests - kamangir/bolt#746

--- .abcli/tests/workflow_runner.sh
+++ .abcli/tests/workflow_runner.sh
@@ -4,6 +4,18 @@ function test_notebooks_and_scripts_worflow_runner() {
     local options=$1
     local do_dryrun=$(abcli_option_int "$options" dryrun 0)
 
-    abcli_eval dryrun=$do_dryrun \
-        "notebooks_and_scripts version ${@:2}"
+    local pattern
+    for pattern in $(echo $NBS_PATTRENS_LIST | tr + " "); do
+        abcli_log "testing pattern=$pattern"
+
+        local object_name=$pattern-$(abcli_string_timestamp)
+
+        workflow create pattern=$pattern $object_name
+
+        workflow submit to=aws_batch $object_name
+
+        workflow monitor ~download $job_name
+
+        abcli_hr
+    done
 }
