596fcf9213101aebc9a8501e65ac00a942af7f15
kamangir <arash@kamangir.net>
2024-05-24 18:52:02 -0700

    refactor - kamangir/bolt#746

--- .abcli/install/notebooks-and-scripts.sh
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env bash
-
-function abcli_install_notebooks_and_scripts() {
-    :
-}
-
-abcli_install_module notebooks_and_scripts 101
--- notebooks_and_scripts/__init__.py
+++ notebooks_and_scripts/__init__.py
@@ -4,4 +4,4 @@ ICON = "📜"
 
 DESCRIPTION = f"{ICON} notebooks and scripts for ai experiments and aws batch jobs."
 
-VERSION = "4.454.1"
+VERSION = "4.455.1"
