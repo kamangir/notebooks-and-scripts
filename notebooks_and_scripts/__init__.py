f62984566a958369935a71cca866307d55e248b7
kamangir <arash@kamangir.net>
2024-06-15 22:30:35 -0700

    += marquee - kamangir/bolt#746

--- notebooks_and_scripts/__init__.py
+++ notebooks_and_scripts/__init__.py
@@ -4,6 +4,8 @@ ICON = "📜"
 
 DESCRIPTION = f"{ICON} notebooks and scripts for ai experiments and aws batch jobs."
 
-VERSION = "4.555.1"
+VERSION = "4.556.1"
 
 REPO_NAME = "notebooks-and-scripts"
+
+MARQUEE = "https://github.com/kamangir/assets/raw/main/nbs/3x4.jpg?raw=true"
