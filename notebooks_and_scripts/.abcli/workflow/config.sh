7062f2ee9620368d3820c1a1acaf6eeb857d39d1
kamangir <arash@kamangir.net>
2024-08-07 18:02:24 -0700

    workflow refactor - kamangir/bolt#746

--- notebooks_and_scripts/.abcli/workflow/config.sh
+++ notebooks_and_scripts/.abcli/workflow/config.sh
@@ -1,20 +1,7 @@
 #! /usr/bin/env bash
 
-function notebooks_and_scripts_workflow_config() {
-    local options=$1
-    local use_cache=$(abcli_option_int "$options" cache 1)
+export NBS_RUNNERS_LIST=$(python3 -m notebooks_and_scripts.workflow.runners list --delim \|)
 
-    export NBS_RUNNERS_LIST="aws_batch|generic|local|localflow"
-    [[ "$use_cache" == 0 ]] &&
-        export NBS_RUNNERS_LIST=$(python3 -m notebooks_and_scripts.workflow.runners list --delim \|)
+export NBS_PATTRENS_LIST=$(python3 -m notebooks_and_scripts.workflow.patterns list --delim \|)
 
-    export NBS_PATTRENS_LIST="a-bc-d|hourglass|map-reduce"
-    [[ "$use_cache" == 0 ]] &&
-        export NBS_PATTRENS_LIST=$(python3 -m notebooks_and_scripts.workflow.patterns list --delim \|)
-
-    export NBS_PATTREN_DEFAULT="a-bc-d"
-    [[ "$use_cache" == 0 ]] &&
-        export NBS_PATTREN_DEFAULT=$(python3 -m notebooks_and_scripts.workflow.patterns list --count 1)
-}
-
-notebooks_and_scripts_workflow_config "$@"
+export NBS_PATTREN_DEFAULT=$(python3 -m notebooks_and_scripts.workflow.patterns list --count 1)
