2ddfdffb38f59e319c125b11dd0abf5f151fe92f
kamangir <arash@kamangir.net>
2024-08-07 20:48:31 -0700

    test ++ - kamangir/bolt#746

--- notebooks_and_scripts/.abcli/tests/workflow_runner.sh
+++ notebooks_and_scripts/.abcli/tests/workflow_runner.sh
@@ -5,17 +5,23 @@ function test_notebooks_and_scripts_worflow_runner() {
     local do_dryrun=$(abcli_option_int "$options" dryrun 0)
 
     local pattern
-    for pattern in $(echo $NBS_PATTRENS_LIST | tr \| " "); do
-        abcli_log "testing pattern=$pattern"
+    local runner
+    for runner in $(echo $NBS_RUNNERS_LIST | tr \| " "); do
+        for pattern in $(echo $NBS_PATTRENS_LIST | tr \| " "); do
+            abcli_log "testing pattern=$pattern"
 
-        local job_name=$pattern-$(abcli_string_timestamp)
+            local job_name=$pattern-$(abcli_string_timestamp)
 
-        workflow create pattern=$pattern $job_name
+            workflow create pattern=$pattern $job_name
+            [[ $? -ne 0 ]] && return 1
 
-        workflow submit ~download,to=aws_batch $job_name
+            workflow submit ~download,to=$runner $job_name
+            [[ $? -ne 0 ]] && return 1
 
-        workflow monitor ~download $job_name
+            workflow monitor ~download $job_name
+            [[ $? -ne 0 ]] && return 1
 
-        abcli_hr
+            abcli_hr
+        done
     done
 }
