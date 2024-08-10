9d63c8bd48983ec4699904173bfc4e860d5d3bb5
kamangir <arash@kamangir.net>
2024-08-09 18:44:00 -0700

    pipeline fix, hopefully - kamangir/bolt#746

--- notebooks_and_scripts/tests/test_workflow_runners.py
+++ notebooks_and_scripts/tests/test_workflow_runners.py
@@ -1,9 +1,8 @@
 import pytest
 from abcli.modules.objects import unique_object
-from notebooks_and_scripts.workflow.runners import list_of_runners
+from notebooks_and_scripts.workflow.runners import list_of_runners, RunnerType
 from notebooks_and_scripts.workflow.generic import Workflow
 from notebooks_and_scripts.workflow.patterns import list_of_patterns
-from notebooks_and_scripts.workflow.runners import RunnerType
 from notebooks_and_scripts.workflow.runners.factory import runner_class
 from notebooks_and_scripts.workflow.runners.generic import GenericRunner
 
@@ -30,7 +29,7 @@ def test_workflow_runners(
 
     assert workflow.load_pattern(pattern)
 
-    runner = runner_class[runner_type]()
+    runner = runner_class[[RunnerType[runner_type.upper()]]]()
     assert isinstance(runner, GenericRunner)
 
     assert runner.submit(workflow, dryrun=True)
