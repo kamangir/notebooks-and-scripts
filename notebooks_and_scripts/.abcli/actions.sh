febbdedac513a5913e180cdfc06720e33dcfb8d0
kamangir <arash@kamangir.net>
2024-10-13 13:14:13 -0700

    git refactor - kamangir/bolt#746

--- notebooks_and_scripts/.abcli/actions.sh
+++ notebooks_and_scripts/.abcli/actions.sh
@@ -2,7 +2,10 @@
 
 function notebooks_and_scripts_action_git_before_push() {
     notebooks_and_scripts build_README
+    [[ $? -ne 0 ]] && return 1
 
     [[ "$(abcli_git get_branch)" == "main" ]] &&
-        notebooks_and_scripts pypi build
+        return 0
+
+    notebooks_and_scripts pypi build
 }
